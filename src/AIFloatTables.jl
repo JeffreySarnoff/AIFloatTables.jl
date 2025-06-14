7d7d6f7760a8142787408975d93790c9f1af7056
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-06-14 09:28:24 -0400

    Update AIFloatTables.jl

--- src/AIFloatTables.jl
+++ src/AIFloatTables.jl
@@ -3,11 +3,14 @@ module AIFloatTables
 using Printf, Tables, DataFrames, CSV, Latexify, PrettyTables
 using AIFloats
 
-const V8   = Vector{UInt8}
-const V16  = Vector{UInt16}
+const U8   = Vector{UInt8}
+const U16  = Vector{UInt16}
 
+const V16 = Vector{Float16}
 const V32 = Vector{Float32}
 const V64 = Vector{Float64}
+
+const B16 = Vector{BFloat16}
 
 #=
 julia> @sprintf("%#04x",0x25)
@@ -32,13 +35,13 @@ colnames = ["uf41", "uf42", "uf43", "uf44"];
 colsyms  = map(Symbol, colnames);
 
 values = [
-   [0.0, 0.0078125, 0.015625, 0.03125, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, NaN],
-   [0.0, 0.0625, 0.125, 0.1875, 0.25, 0.375, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0, 8.0, NaN],
-   [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, NaN],
-   [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, NaN]
+   Float32[0.0, 0.0078125, 0.015625, 0.03125, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, NaN],
+   Float32[0.0, 0.0625, 0.125, 0.1875, 0.25, 0.375, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0, 8.0, NaN],
+   Float32[0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, NaN],
+   Float32[0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, NaN]
 ];
 
-nt4table = NamedTuple{Tuple(colsyms), NTuple{4,V64}};
+nt4table = NamedTuple{Tuple(colsyms), NTuple{4,V32}};
 nt = nt4table(values);
 coltable = columntable(nt)
 
@@ -56,7 +59,7 @@ values2 = [
   [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, NaN]
 ];
 
-nt4table2 = NamedTuple{Tuple(colsyms2), Tuple{V8,V64,V64,V64,V64}};
+nt4table2 = NamedTuple{Tuple(colsyms2), Tuple{V8,V32,V32,V32,V32}};
 nt2 = nt4table2(values2);
 coltable2 = columntable(nt2);
 
