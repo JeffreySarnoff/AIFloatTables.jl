d21ec1e409552b46f9484c05acbe63a6dfc45b9b
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-06-23 23:04:06 -0400

    update

--- src/AIFloatTables.jl
+++ src/AIFloatTables.jl
@@ -23,11 +23,15 @@ rstrip0(x) = rstrip(@sprintf("%.7f",x),'0') * (isinteger(x) ? '0' : "")
 =#
 
 hex_formatter = (v, i, j) -> j==1 ? ifelse(isa(v, UInt8), @sprintf("%#04x", v), @sprintf("%#06x", v)) : v
+float_formatter = (v, i, j) ->  string(@sprintf("%#a",v)," (", @sprintf("%#g", v), ") ")
+formatters = (v, i, j) -> ifelse(j==1, hex_formmater(v), float_formatter(v))
 
+
+formats=(v,i,j)->ifelse(j>1, round(v; sigdigits=16), hex_formatter(v,i,j))
 rstrip0(x) = rstrip(@sprintf("%.7f",x),'0') * (isinteger(x) ? '0' : "")
 
-float_formatter = (v, i, j) -> rstrip0(v)
-formatters = (hex_formatter, float_formatter)
+
+formatters = (hex_formatter, float_formatter, float_formatter, float_formatter, float_formatter);
 
 encoding = collect(0x00:0x0f);
 
@@ -45,6 +49,8 @@ nt4table = NamedTuple{Tuple(colsyms), NTuple{4,V32}};
 nt = nt4table(values);
 coltable = columntable(nt)
 
+pretty_table(coltable; formatters = formats, alignment=:l);
+
 pretty_string = pretty_table(String, coltable; header = colnames);
 pretty_html   = pretty_table(HTML, coltable; header = colnames);
 
@@ -59,16 +65,39 @@ values2 = [
   [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, NaN]
 ];
 
-nt4table2 = NamedTuple{Tuple(colsyms2), Tuple{V8,V32,V32,V32,V32}};
+nt4table2 = NamedTuple{Tuple(colsyms2), Tuple{U8,V32,V32,V32,V32}};
 nt2 = nt4table2(values2);
 coltable2 = columntable(nt2);
 
-pretty_table2  = pretty_table(coltable2; alignment=:l)
-pretty_string2 = pretty_table(String, coltable2);
-pretty_html2   = pretty_table(HTML, coltable2);
+pretty_table2  = pretty_table(coltable2; formatters=formats, alignment=:l)
+pretty_string2 = pretty_table(String, coltable2; formatters=formats, alignment=:l);
+pretty_html2   = pretty_table(HTML, coltable2; formatters=formats, alignment=:l);
 
 
 valuematrix = reshape(collect(Iterators.flatten(values)),(4^2,4)); 
 valuetable = Tables.table(valuematrix);
+
+hexform=(col,val)->@sprintf("%#04x",val)
+floatform = (col, val) -> @sprintf("%g",val)
+form(col, val) = ifelse(col == 1, hexform(col, val), floatform(col, val))
+rowitr = CSV.RowWriter(coltable2;transform=form);for r in rowitr;print(r); end
+
+ufA1 = AIFloat(10,1;signed=true,extended=false);
+ufA9 = AIFloat(10,9;signed=true,extended=false);
+ufAA = AIFloat(10,10;signed=true,extended=false);
+
+sfA1 = AIFloat(10,1;signed=true,extended=false);
+sfA9 = AIFloat(10,9;signed=true,extended=false);
+
+colnames = [:code, :ufA1, :sfA1, :ufA9, :sfA9];
+values = [ 
+  codes(ufA1), floats(ufA1), floats(sfA1), floats(ufA9), floats(sfA9)
+];
+
+nt4table = NamedTuple{Tuple(colnames), Tuple{U16,V64,V64,V64,V64}};
+nt = nt4table(values);
+coltable = columntable(nt);
+
+prettytable = pretty_table(coltable; formatters=formats, alignment=:l)
 
 end  # AIFloatTables
