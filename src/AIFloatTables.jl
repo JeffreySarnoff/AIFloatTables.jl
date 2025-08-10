53f62576ddf54e3367dfa00d4f81c3d6ccc53108
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-08-09 23:55:52 -0400

    pkg version

--- src/AIFloatTables.jl
+++ src/AIFloatTables.jl
@@ -11,7 +11,7 @@ localdir = s"P3109"
 minbits, maxbits = 2, 16
 dirs = make_directories(basedir, localdir; minbits, maxbits);
 
-generate_files(dirs; minbits, maxbits)
-
+# generate_files(dirs; minbits, maxbits)
+println("generate_files(dirs; minbits, maxbits) is commented out to avoid accidental generation of files.")
 
 end # module AIFloatTables
